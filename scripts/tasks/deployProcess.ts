aad160dfc5a3e8a2488d887bba4682bd50063641
cheng-chun-yuan <83824908+cheng-chun-yuan@users.noreply.github.com>
2024-03-27 16:54:50 +0800

    read csv user_address

--- scripts/tasks/deployProcess.ts
+++ scripts/tasks/deployProcess.ts
@@ -1,9 +1,39 @@
 import { task } from "hardhat/config"
 import { readFileSync, writeFileSync } from "../helpers/pathHelper"
+import * as fs from "fs";
+import * as path from "path";
+import { parse } from 'csv-parse';
+type CSVFormat = {
+  name: string,
+  address: string,
+  nickname: string,
+  background: string,
+  telegram: string
+};
+  const csvFilePath = path.resolve(__dirname, '../../photo_overlap/airdrop.csv');
+
+  const headers = ['name', 'address', 'nickname', 'background', 'telegram'];
+
+  const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' });
+
 
 task("deploy:member", "Deploy member")
   .addFlag("verify", "Validate contract after deploy")
   .setAction(async ({ verify }, hre) => {
+    const addressArray = [] as string[]
+    parse(fileContent, {
+      delimiter: ',',
+      columns: headers,
+    }, (error, result: CSVFormat[]) => {
+      if (error) {
+        console.error(error);
+      }
+      result.forEach((row) => {
+        if (row.address == "address")
+          return
+        addressArray.push(row.address)
+      });
+    });
     await hre.run("compile")
     const [signer]: any = await hre.ethers.getSigners()
     const feeData = await hre.ethers.provider.getFeeData()
@@ -17,35 +47,9 @@ task("deploy:member", "Deploy member")
       maxFeePerGas: feeData.maxFeePerGas,
       gasLimit: 6000000, // optional: for some weird infra network
     })
-
     console.log(`membership.sol deployed to ${XueDAODeployContract.address}`)
     const airdrop = await XueDAODeployContract.airdrop(
-      [
-        "0x099Bb36F9D41F6Eca738b9854A8F07BA02Bf3E48",
-        "0x9DB36029198CD3Dc70DE207be6918558AB0b70ea",
-        "0xF092Dc31b7410c16Ffd0c43068B430e38F725150",
-        "0xAa83c234Eb8125CB2485bEE6aD98da0A4892405A",
-        "0xdA048C2845E7696cEB805FAF116F036a046A73f1",
-        "0xD49EeDbE1287C7C8d7a1906477de4184e7988dBD",
-        "0xf12382CCa36Aa958f3fD3A24C3C3B33BB79c5079",
-        "0xC824D8D6AdA95eA00B0258d080C31663DA1abAd1",
-        "0x8C4bC2521E315E4fd7c29B3d0F00f74aDCAF4Bb9",
-        "0x579C1aF91BD134Ed42a79393c058fCd9316555E3",
-        "0x063c11FdC4fC65D9B87605202194423C0f49aC0A",
-        "0xd6D2991e9388E21aF8afF27d7e552A99D4cb512B",
-        "0x1CEB02e12b665006676D7Bb8A5Df0FFC9Df5c261",
-        "0xb7E6904045640890bD6460E74CA8963b7304de57",
-        "0x6FA842C57a92D0DC136a6A76dD13A564515A69cf",
-        "0x70f11cF8f76035CaDfd1b60c0c265e1E8Fe52287",
-        "0x0D2FDDee5b84540A9766c025ad26dCaFb9FeF380",
-        "0x2A01CfA467cb049FDe8849873E976FB7A7FF9B0A",
-        "0xFc910402a530F82755Bf3ea467825448406719b3",
-        "0xf93773a8a0687e5866E44e862d5bb556a4a551f3",
-        "0x538399e9AECF5d6Cb1Eadb4Aa2F48eCF1CF64Ec5",
-        "0xd4c384eC8a9f9EbFc97458833FF0147a131f7057",
-        "0xEbf29A4dc710040B12E68465331F70e42f053d7b",
-        "0x09009fC07c9eAd873D18C358ac71D02E36e1D354",
-      ]
+      addressArray,
     )
     console.log(`airdrop: ${airdrop.hash}`)
     const setBaseURI = await XueDAODeployContract.setBaseURI("https://ipfs.io/ipfs/QmUk3M3xbiAXppHjiJJGQLRFBA77AjYy6BtGHD4gPCtziN/")
